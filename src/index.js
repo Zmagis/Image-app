3599f7e6082bcc22a3e090b02f0387f49d69d737
Egle Gustaite <eglegustaite@gmail.com>
2020-02-09 22:36:04 +0200

    nothing actually changed

--- src/index.js
+++ src/index.js
@@ -10,6 +10,10 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import reducer from "./store/reducers/reducer";
 
+require("dotenv").config();
+
+console.log(process.env);
+
 const composeEnhancers =
   process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
