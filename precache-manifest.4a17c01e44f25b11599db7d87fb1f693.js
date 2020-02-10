9686394c49bc5e2c7f544fa038be9c0a188efab3
Egle Gustaite <eglegustaite@gmail.com>
2020-02-10 10:53:18 +0200

    Updates

---
+++ precache-manifest.4a17c01e44f25b11599db7d87fb1f693.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba228ff456cf42d0e263295e20473a7",
+    "url": "/photoapp/index.html"
+  },
+  {
+    "revision": "8ec414f77a39673507ee",
+    "url": "/photoapp/static/css/main.200ec095.chunk.css"
+  },
+  {
+    "revision": "6b3ca93ec81732db77b9",
+    "url": "/photoapp/static/js/2.32efe5c3.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/photoapp/static/js/2.32efe5c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec414f77a39673507ee",
+    "url": "/photoapp/static/js/main.5951163b.chunk.js"
+  },
+  {
+    "revision": "d809ad65be1915aba7f1",
+    "url": "/photoapp/static/js/runtime-main.a2ed0ef8.js"
+  }
+]);
